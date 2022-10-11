871b2f606ff0813fd95d7aea1a7584e2a25a630b
laujuu <lauroindcontato@gmail.com>
2022-10-11 14:58:41 -0300

    :tada: init: Iniciando Projeto

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+// #vqv
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
